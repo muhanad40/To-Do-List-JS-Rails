ef56d39f362df907ca74dcced8ee63ce193890f6
muhanad <muhanad40@gmail.com>
2014-05-28 17:56:37 +0100

    Installed Devise

---
+++ spec/features/to_do_feature_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'To Do App' do
+
+	context 'When not logged in' do
+
+		it 'redirects the user to the log in page' do
+			visit '/'
+			expect(current_path).to have_content '/login'
+		end
+
+	end
+
+end
