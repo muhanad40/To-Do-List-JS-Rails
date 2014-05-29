92c6f0ae7aa56f3e068d9b5dec6530705f67e12c
muhanad <muhanad40@gmail.com>
2014-05-29 11:11:40 +0100

    Added log in and out tests, passed

---
+++ spec/features/log_in_out_feature_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'To Do App' do
+
+	context 'When not logged in' do
+
+		it 'redirects the user to the log in page' do
+			visit '/'
+			expect(current_path).to have_content '/users/sign_in'
+		end
+
+	end
+
+	context 'When logged in' do
+
+		before(:each) do
+			user = User.create(full_name: 'Muhanad Al-Rubaiee', email: 'muhanad@gmail.com', password: '12345678', password_confirmation: '12345678')
+			login_as user
+		end
+
+		it 'Displays the full name of the logged in user' do
+			visit '/'
+			expect(current_path).to have_content '/'
+			expect(page).to have_content 'Muhanad Al-Rubaiee'
+		end
+
+	end
+
+end
