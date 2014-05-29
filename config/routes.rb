acb3ac11627a89d8d026fe9ef9412bfebfbf41f0
muhanad <muhanad40@gmail.com>
2014-05-29 16:19:19 +0100

    Minor change and correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  resources :todo
-  root 'todo#index'
+  devise_for :user
+  resources :task
+  root 'task#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
