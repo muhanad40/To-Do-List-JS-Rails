8e2867629fa79d9f8af8e3fd2a0d2e6806f55203
muhanad <muhanad40@gmail.com>
2014-05-29 11:15:30 +0100

    Test for User model to add full name, passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'Adds user full name' do
+  	user = User.create(
+  		full_name: 'Muhanad Al-Rubaiee',
+  		email: 'muhanad@gmail.com',
+  		password: '12345678',
+  		password_confirmation: '12345678'
+  	)
+  	expect(user.full_name).to eq 'Muhanad Al-Rubaiee'
+  end
+
 end
