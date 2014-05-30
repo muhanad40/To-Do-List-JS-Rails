8748079af7f2265ed868001125ad7a542bdaf59c
muhanad <muhanad40@gmail.com>
2014-05-30 12:42:13 +0100

    Added create action for tasks

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -4,4 +4,10 @@ class TaskController < ApplicationController
 		authenticate_user!
 	end
 
+	def create
+		authenticate_user!
+		@task = current_user.tasks.create(task: params[:task], status: params[:status], user_id: current_user.id, order: params[:order])
+		render :json => @task
+	end
+
 end
