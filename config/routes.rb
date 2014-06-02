1a58a88c3883d22b8d0ef240b476da9919cd6cc1
muhanad <muhanad40@gmail.com>
2014-06-02 16:30:37 +0100

    Added tasks reorder route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :user
   resources :task
   root 'task#index'
-  get 'tasks' => 'task#all_tasks'
+  patch 'tasks/reorder', to: 'task#reorder'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
