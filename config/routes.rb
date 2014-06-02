d03323ef75e20e360b947d28d0f67dcca8d6d8e9
muhanad <muhanad40@gmail.com>
2014-06-02 12:39:46 +0100

    Added tasks route to json response

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :user
   resources :task
   root 'task#index'
+  get 'tasks' => 'task#all_tasks'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
